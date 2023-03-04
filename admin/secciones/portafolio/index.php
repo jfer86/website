fa82f4513d49b4690f3820c4c6d093129ca132f6
JuanFe86 <60895251+JuanFe86@users.noreply.github.com>
2023-03-04 16:36:42 -0500

    se crea tabla base

--- admin/secciones/portafolio/index.php
+++ admin/secciones/portafolio/index.php
@@ -1,3 +1,47 @@
 <?php include("../../templates/headers.php"); ?>
-Listar portafolios
+
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Registros</a>
+    </div>
+    <div class="card-body">
+
+        <div class="table-responsive-sm">
+            <table class="table table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Titulo</th>
+                        <th scope="col">Subtitulo</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">url</th>
+                        <th scope="col">acciones</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="col">1</td>
+                        <td scope="col">Software restaurante</td>
+                        <td scope="col">Software para su restaurante a la medida</td>
+                        <td scope="col">imagen.jpeg</td>
+                        <td scope="col">Software para su restaurante a la medida realizado con PHP</td>
+                        <td scope="col">Restaurante slayer</td>
+                        <td scope="col">Software</td>
+                        <td scope="col">http://google.com</td>
+                        <td scope="col">Editar | Eliminar</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+
+</div>
+
 <?php include("../../templates/footer.php"); ?>
