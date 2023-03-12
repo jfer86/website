6c848e039e698e90d7bb34967005898f55d3f32b
JuanFe86 <60895251+JuanFe86@users.noreply.github.com>
2023-03-12 18:26:53 -0500

    se crea input de entradas

--- admin/secciones/entradas/crear.php
+++ admin/secciones/entradas/crear.php
@@ -1,3 +1,50 @@
-<?php include("../../templates/headers.php"); ?>
-Crear entradas
+<?php
+include("../../bd.php");
+
+if($_POST){
+
+    print_r($_POST); 
+    print_r($_FILES);
+}
+include("../../templates/headers.php"); ?>
+
+<div class="card">
+    <div class="card-header">
+        Entradas
+    </div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="fecha" class="form-label">Fecha:</label>
+                <input type="date" class="form-control" name="fecha" id="fecha" aria-describedby="helpId" placeholder="Fecha">
+            </div>
+
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo:</label>
+                <input type="text" class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Titulo">
+            </div>
+
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripcion: </label>
+                <input type="text" class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripcion">
+            </div>
+
+            <div class="mb-3">
+                <label for="imagen" class="form-label">Imagen:</label>
+                <input type="file" class="form-control" name="imagen" id="imagen" aria-describedby="helpId" placeholder="Imagen">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+
+
+
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+
 <?php include("../../templates/footer.php"); ?>
