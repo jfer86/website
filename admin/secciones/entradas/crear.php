541fa9a3dc6532230e26594a153020c36cb7f56e
JuanFe86 <60895251+JuanFe86@users.noreply.github.com>
2023-03-12 18:38:38 -0500

    se crea conexion con bd y se agregan imagenes

--- admin/secciones/entradas/crear.php
+++ admin/secciones/entradas/crear.php
@@ -3,8 +3,31 @@ include("../../bd.php");
 
 if($_POST){
 
-    print_r($_POST); 
-    print_r($_FILES);
+    //recepcionamos los valores del formulario
+    $fecha = (isset($_POST['fecha'])) ? $_POST['fecha'] : "";
+    $titulo = (isset($_POST['titulo'])) ? $_POST['titulo'] : "";
+    $descripcion = (isset($_POST['descripcion'])) ? $_POST['descripcion'] : "";
+    $imagen = (isset($_FILES['imagen']["name"]))? $_FILES['imagen']["name"]: "";
+
+    $fecha_imagen=new DateTime();
+    $nombre_archivo_imagen=($imagen!="" )? $fecha_imagen->getTimestamp()."_".$imagen:"";
+    $tmp_imagen=$_FILES["imagen"]["tmp_name"];
+    if($tmp_imagen!=""){
+        move_uploaded_file($tmp_imagen,"../../../assets/img/about/".$nombre_archivo_imagen);
+    }
+
+    
+    $sentencia = $conexion->prepare("INSERT INTO `tbl_entradas`
+    (`ID`, `fecha`, `titulo`, `descripcion`, `imagen`)
+    VALUES (NULL, :fecha, :titulo, :descripcion, :imagen);");
+
+    $sentencia->bindParam(':fecha', $fecha);
+    $sentencia->bindParam(':titulo', $titulo);
+    $sentencia->bindParam(':descripcion', $descripcion);
+    $sentencia->bindParam(':imagen', $nombre_archivo_imagen);
+    $sentencia->execute();
+    $mensaje = "Datos agregados correctamente";
+    header("Location: index.php?mensaje=$mensaje");
 }
 include("../../templates/headers.php"); ?>
 
