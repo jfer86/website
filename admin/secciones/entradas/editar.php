3f68662e86647aa3cb70886d30f11d9e471e8994
JuanFe86 <60895251+JuanFe86@users.noreply.github.com>
2023-03-12 19:04:15 -0500

    se carga informacion para editar

--- admin/secciones/entradas/editar.php
+++ admin/secciones/entradas/editar.php
@@ -1,3 +1,62 @@
-<?php include("../../templates/headers.php"); ?>
-Editar entradas
+<?php
+include("../../bd.php");
+
+if (isset($_GET['txtID'])) {
+    $txtID = (isset($_GET['txtID'])) ? $_GET['txtID'] : "";
+    $sentencia = $conexion->prepare("SELECT * FROM `tbl_entradas` WHERE ID = :ID");
+    $sentencia->bindParam(':ID', $txtID);
+    $sentencia->execute();
+    $registro = $sentencia->fetch(PDO::FETCH_LAZY);
+
+    $fecha = $registro['fecha'];
+    $titulo = $registro['titulo'];
+    $descripcion = $registro['descripcion'];
+    $imagen = $registro['imagen'];
+        
+}
+
+
+include("../../templates/headers.php");
+?>
+
+<div class="card">
+    <div class="card-header">
+        Entradas
+    </div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="fecha" class="form-label">Fecha:</label>
+                <input type="date" class="form-control" value="<?php echo $fecha;?>" name="fecha" id="fecha" aria-describedby="helpId" placeholder="Fecha">
+            </div>
+
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo:</label>
+                <input type="text" class="form-control" value="<?php echo $titulo;?>" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Titulo">
+            </div>
+
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripcion: </label>
+                <input type="text" class="form-control" value="<?php echo $descripcion;?>" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripcion">
+            </div>
+
+            <div class="mb-3">
+                <label for="imagen" class="form-label">Imagen:</label>
+                <img width="50" src="../../../assets/img/about/<?php echo $imagen; ?>" />
+                <input type="file" class="form-control" name="imagen" id="imagen" aria-describedby="helpId" placeholder="Imagen">
+            </div>
+
+            <button type="submit" class="btn btn-success">Actualizar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+
+
+
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+
 <?php include("../../templates/footer.php"); ?>
