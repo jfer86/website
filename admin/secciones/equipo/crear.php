d3cdafd7965114324dfb9752ce0e6a36c7a1d450
JuanFe86 <60895251+JuanFe86@users.noreply.github.com>
2023-03-18 08:09:58 -0500

    se crean las interfaces de equipo

--- admin/secciones/equipo/crear.php
+++ admin/secciones/equipo/crear.php
@@ -1,3 +1,53 @@
-<?php include("../../templates/headers.php"); ?>
-Crear equipo
+<?php
+include("../../bd.php");
+include("../../templates/headers.php"); ?>
+
+<div class="card">
+    <div class="card-header">
+        Datos del Equipo
+    </div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="imagen" class="form-label">Imagen: </label>
+                <input type="file" class="form-control" name="imagen" id="imagen" aria-describedby="helpId" placeholder="Imagen">
+            </div>
+
+            <div class="mb-3">
+                <label for="nombrecompleto" class="form-label">Nombre completo</label>
+                <input type="text" class="form-control" name="nombrecompleto" id="nombrecompleto" aria-describedby="helpId" placeholder="Nombre completo">
+            </div>
+
+            <div class="mb-3">
+                <label for="puesto" class="form-label">Puesto:</label>
+                <input type="text" class="form-control" name="puesto" id="puesto" aria-describedby="helpId" placeholder="Puesto">
+            </div>
+
+            <div class="mb-3">
+                <label for="Twitter" class="form-label">Twiteer:</label>
+                <input type="text" class="form-control" name="Twitter" id="Twitter" aria-describedby="helpId" placeholder="Twitter">
+            </div>
+
+            <div class="mb-3">
+                <label for="facebook" class="form-label">Facebook:</label>
+                <input type="text" class="form-control" name="facebook" id="facebook" aria-describedby="helpId" placeholder="Faceboook">
+            </div>
+
+            <div class="mb-3">
+                <label for="linkedin" class="form-label">linkedin:</label>
+                <input type="text" class="form-control" name="linkedin" id="linkedin" aria-describedby="helpId" placeholder="linkedin">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+        </form>
+
+    </div>
+    <div class="card-footer text-muted">
+        Footer
+    </div>
+</div>
+
 <?php include("../../templates/footer.php"); ?>
--- admin/secciones/equipo/index.php
+++ admin/secciones/equipo/index.php
@@ -1,3 +1,53 @@
-<?php include("../../templates/headers.php"); ?>
-Listar equipo
+<?php
+include("../../bd.php");
+
+//Seleccionar registros
+$sentencia = $conexion->prepare("SELECT * FROM `tbl_equipo`");
+$sentencia->execute();
+$listaEntradas = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+include("../../templates/headers.php");
+?>
+<div class="card">
+    <div class="card-header">
+    <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Registros</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col">nombrecompleto</th>
+                        <th scope="col">puesto</th>
+                        <th scope="col">twitter</th>
+                        <th scope="col">facebook</th>
+                        <th scope="col">linkedin</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td>R1C1</td>
+                        <td>Imagen.jpg</td>
+                        <td>Juan</td>
+                        <td>CEo</td>
+                        <td>twitter</td>
+                        <td>facebook</td>
+                        <td>linkedin</td>
+                        <td>Editar | eliminar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+    <div class="card-footer text-muted">
+        Footer
+    </div>
+</div>
+
 <?php include("../../templates/footer.php"); ?>
